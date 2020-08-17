c8fae27fc858805c1768c444e7e557efe683d8ba
Leon Linhart <themrmilchmann@gmail.com>
2020-08-17 22:21:42 +0200

    chore: update Copyright headers

--- buildSrc/src/main/kotlin/com/github/themrmilchmann/build/Deployment.kt
+++ buildSrc/src/main/kotlin/com/github/themrmilchmann/build/Deployment.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2019 Leon Linhart
+ * Copyright 2018-2020 Leon Linhart
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
