081391324a3d823a2cbae2e37587ec67538ac91f
Leon Linhart <themrmilchmann@gmail.com>
2020-11-22 12:35:36 +0100

    build: update Gradle JMH plugin {0.5.0 => 0.5.2}

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ import org.gradle.internal.jvm.*
 
 plugins {
     `java-library`
-    id("me.champeau.gradle.jmh") version "0.5.0"
+    id("me.champeau.gradle.jmh") version "0.5.2"
     signing
     `maven-publish`
 }
