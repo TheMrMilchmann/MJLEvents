6a813dda20853b75062d01dd3763c87080449563
Leon Linhart <themrmilchmann@gmail.com>
2021-01-08 22:46:32 +0100

    chore: update copyright

--- src/main/java/com/github/themrmilchmann/mjl/events/EventDispatcher.java
+++ src/main/java/com/github/themrmilchmann/mjl/events/EventDispatcher.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2020 Leon Linhart
+ * Copyright 2018-2021 Leon Linhart
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
